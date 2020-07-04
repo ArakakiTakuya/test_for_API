a5bf88ee6355a973900a75e6b142530a3d9f2f13
ArakakiTakuya <225arataku@gmail.com>
2020-07-05 00:37:09 +0900

    rewrote to get reviews info

--- src/App.js
+++ src/App.js
@@ -56,6 +56,33 @@ function App() {
     "沖縄県",
   ];
 
+  async function getReview(location_id) {
+    const res = await axios({
+      method: "GET",
+      url: "https://tripadvisor1.p.rapidapi.com/reviews/list",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+        "x-rapidapi-key": "YOUR_API_KEY", // APIキーを入力。
+        useQueryString: true,
+      },
+      params: {
+        limit: "5", // 今は5件の口コミ情報が取得できる。
+        lang: "ja_JP",
+        location_id: location_id,
+      },
+    });
+    const data = res.data.data.map((review) => {
+      return {
+        title: review.title,
+        text: review.text,
+        rating: review.rating,
+        published_date: review.published_date,
+      };
+    });
+    return data;
+  }
+
   async function getData() {
     const res = await axios({
       method: "GET",
@@ -67,31 +94,33 @@ function App() {
         useQueryString: true,
       },
       params: {
-        limit: "30",
-        offset: "0", //どこからどこまでか。今は0~29
+        limit: "9", //一度のアクセスを減らすために9へ。10以上にするとアクセスしすぎのエラーが出る。
+        offset: "0", //どこからどこまでか。今は0~8
         lang: "ja_JP",
         query: prefecture[0], //都道府県名を入力
       },
     });
 
-    const data = res.data.data
-      .filter((obj) => obj.result_type === "things_to_do") //ここでフィルター。今は観光スポットが取れてる。lodgingでホテル。restaurantsでレストラン情報が取れる。
-      .map((obj) => {
-        return {
-          name: obj.result_object.name,
-          location: {
-            lat: Number(obj.result_object.latitude),
-            lng: Number(obj.result_object.longitude),
-          },
-          image: obj.result_object.photo["images"]["small"]["url"],
-          category: obj.result_object.category["name"],
-          rating: obj.result_object.rating,
-          //review: obj.review_snippet["snippet"], // 観光地説明のようなもの。これがある県とない県があるため今はコメントアウト中。
-        };
-      });
-
+    const data = await Promise.all(
+      res.data.data
+        .filter((obj) => obj.result_type === "things_to_do") //ここでフィルター。今は観光スポットが取れてる。lodgingでホテル。restaurantsでレストラン情報が取れる。
+        .map(async (obj) => {
+          return {
+            name: obj.result_object.name,
+            location: {
+              lat: Number(obj.result_object.latitude),
+              lng: Number(obj.result_object.longitude),
+            },
+            image: obj.result_object.photo["images"]["small"]["url"],
+            category: obj.result_object.category["name"],
+            rating: obj.result_object.rating,
+            reviews: await getReview(obj.result_object.location_id),
+          };
+        })
+    );
     console.log(data);
     console.log(JSON.stringify(data));
+    return data;
   }
 
   return <div className="App"></div>;
