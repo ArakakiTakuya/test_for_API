281830b68decd84dc54351d4aee6bcdd97f2c5b1
ArakakiTakuya <225arataku@gmail.com>
2020-07-04 12:30:48 +0900

    got data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+const axios = require("axios");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const prefecture = [
+    "北海道",
+    "青森県",
+    "岩手県",
+    "宮城県",
+    "秋田県",
+    "山形県",
+    "福島県",
+    "茨城県",
+    "栃木県",
+    "群馬県",
+    "埼玉県",
+    "千葉県",
+    "東京都",
+    "神奈川県",
+    "新潟県",
+    "富山県",
+    "石川県",
+    "福井県",
+    "山梨県",
+    "長野県",
+    "岐阜県",
+    "静岡県",
+    "愛知県",
+    "三重県",
+    "滋賀県",
+    "京都府",
+    "大阪府",
+    "兵庫県",
+    "奈良県",
+    "和歌山県",
+    "鳥取県",
+    "島根県",
+    "岡山県",
+    "広島県",
+    "山口県",
+    "徳島県",
+    "香川県",
+    "愛媛県",
+    "高知県",
+    "福岡県",
+    "佐賀県",
+    "長崎県",
+    "熊本県",
+    "大分県",
+    "宮崎県",
+    "鹿児島県",
+    "沖縄県",
+  ];
+
+  async function getData() {
+    const res = await axios({
+      method: "GET",
+      url: "https://tripadvisor1.p.rapidapi.com/locations/search",
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+        "x-rapidapi-key": "YOUR_API_KEY",
+        useQueryString: true,
+      },
+      params: {
+        location_id: "1",
+        limit: "30",
+        sort: "relevance",
+        offset: "0", //どこからどこまでか。今は0~29
+        lang: "ja_JP",
+        currency: "USD",
+        units: "km",
+        query: prefecture[0], //都道府県名を入力
+      },
+    });
+
+    const data = res.data.data
+      .filter((obj) => obj.result_type === "things_to_do")
+      .map((obj) => {
+        return {
+          name: obj.result_object.name,
+          location: {
+            lat: Number(obj.result_object.latitude),
+            lng: Number(obj.result_object.longitude),
+          },
+          image: obj.result_object.photo["images"]["small"]["url"],
+          category: obj.result_object.category["name"],
+          rating: obj.result_object.rating,
+          review: obj.review_snippet["snippet"], // 観光地説明のようなもの。
+        };
+      });
+
+    console.log(JSON.stringify(data));
+  }
+
+  return <div className="App"></div>;
 }
 
 export default App;
